9e00983656cbea43d32eac5d3936a34238845f9b
John Ocampo <john.devsigner@gmail.com>
2023-03-04 13:33:27 -0500

    travel responsive web design

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+    './src/**/*.{html,js}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'primary':"#CC2D4A",
+        'secondary':"#8FA206",
+        'tertiary':"#61AEC9",
+      },
+      fontFamily: {
+        Montserrat: ["Monserrat", "sans-serif"],
+      },
+      backgroundImage: {
+        "sanFrancisco": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/sanFrancisco.jpg')",
+        "sanFranciscoDesktop": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/sanFranciscoDesktop.jpg')",
+        "yosemite": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/yosemite.jpg')",
+        "LA": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/LA.jpg')",
+        "seattle": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/seattle.jpg')",
+        "new_york": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/new_york.jpg')",
+        "norway": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/norway.jpg')",
+        "sydney": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/sydney.jpg')",
+        "miami": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/miami.jpg')",
+        "switzerland": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/switzerland.jpg')",
+        "bali": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/bali.jpg')",
+        "chicago": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/chicago.jpg')",
+        "europe": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/europe.jpg')",
+        "iceland": "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/iceland.jpg')",
+      }
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide'),
+  ],
+}
+}
